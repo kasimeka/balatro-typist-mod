0bde8c45563bf650dee92eb09528bbfaca6be5de
janw4ld <janw4ld@tutanota.com>
2025-02-28 10:42:37 +0200

    fix: bogus conditions making cardarea multiselect impossible
    😳😳

--- cardarea-handler.lua
+++ cardarea-handler.lua
@@ -79,8 +79,7 @@ return function(area, key, held_keys)
     -- if ctrl is held, select the target card as well but only in booster hands
     elseif
       area == G.hand
-      and held_keys[layout.select_multiple_right]
-      and held_keys[layout.select_multiple_left]
+      and (held_keys[layout.select_multiple_right] or held_keys[layout.select_multiple_left])
       and (G.STATE == G.STATES.TAROT_PACK or G.STATE == G.STATES.SPECTRAL_PACK)
     then
       CardArea.toggle_card_by_index(area, target)
