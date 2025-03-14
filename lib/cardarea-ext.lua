681549d1303a4bbf7fbc059f22ae178fa390af2e
janw4ld <janw4ld@tutanota.com>
2025-03-14 07:23:09 +0200

    cardarea-ext: fix unhover timing conflicts, increase unhover delay

--- lib/cardarea-ext.lua
+++ lib/cardarea-ext.lua
@@ -1,3 +1,8 @@
+local last_hover = {}
+local no_op = function()
+  return true
+end
+
 function CardArea:__typist_toggle_card_by_index(index)
   local target = self.cards[index]
   if not target then
@@ -5,24 +10,30 @@ function CardArea:__typist_toggle_card_by_index(index)
     return
   end
 
-  for _, c in ipairs(self.cards) do
-    c:stop_hover()
+  if last_hover.e and not last_hover.e.complete then
+    last_hover.e.complete = true
+    last_hover.card:stop_hover()
+    last_hover.e.func = no_op
   end
 
   target:click()
 
   target:hover()
   if G.E_MANAGER then
-    G.E_MANAGER:add_event(Event {
+    last_hover.card = target
+    last_hover.e = Event {
+      -- while selecting a card, hover it for a longer duration than deselection
+      delay = target.highlighted and 10 or 0.2, -- seconds
+      timer = "REAL",
       trigger = "after",
-      delay = 6, -- seconds
       blockable = false,
       blocking = false,
       func = function()
         target:stop_hover()
         return true
       end,
-    })
+    }
+    G.E_MANAGER:add_event(last_hover.e)
   end
 
   return true
