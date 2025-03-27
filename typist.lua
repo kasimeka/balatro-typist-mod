bae4a581d59f7178470ebce4e9e9a99761446792
Jack Mahoney <jacksmahoney@gmail.com>
2025-03-26 23:23:51 -0700

    feat: add smod author/description and mod icon

---
+++ typist.lua
@@ -0,0 +1,16 @@
+--- STEAMODDED HEADER
+
+--- MOD_NAME: Typist
+--- MOD_ID: typist
+--- MOD_AUTHOR: [janw4ld]
+--- MOD_DESCRIPTION: Keyboard shortcuts for card manipulation and hand assessment
+--- PREFIX: typist
+--- VERSION: 1.0.0
+--- LOADER_VERSION_GEQ: 1.0.0
+
+SMODS.Atlas {
+    key = "modicon",
+    path = "avatar.png",
+    px = 34,
+    py = 34
+}
