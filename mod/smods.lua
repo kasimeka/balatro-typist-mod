af4f90f21260d4339017a7205b6bf248a917f07d
janw4ld <janw4ld@tutanota.com>
2025-03-27 09:31:57 +0200

    rename smods files from `typist` to `smods`

---
+++ mod/smods.lua
@@ -0,0 +1,5 @@
+if SMODS then
+  SMODS.Atlas { key = "modicon", path = "avatar.png", px = 34, py = 34 }
+else
+  print("This file is meaningless without the `SMODS` runtime")
+end
--- typist.lua
+++
@@ -1,7 +0,0 @@
-
-SMODS.Atlas {
-    key = "modicon",
-    path = "avatar.png",
-    px = 34,
-    py = 34
-}
