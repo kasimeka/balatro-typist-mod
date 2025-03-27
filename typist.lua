bcb547df649c456b88cf35d0c50355803a38f441
Jack Mahoney <jacksmahoney@gmail.com>
2025-03-27 00:02:05 -0700

    refactor: migrate to JSON metadata and remove legacy header

--- typist.lua
+++ typist.lua
@@ -1,12 +1,3 @@
---- STEAMODDED HEADER
-
---- MOD_NAME: Typist
---- MOD_ID: typist
---- MOD_AUTHOR: [janw4ld]
---- MOD_DESCRIPTION: Fully keyboard-driven UX with keybindings for (almost) every in-game action.
---- PREFIX: typist
---- VERSION: 1.3.0
---- LOADER_VERSION_GEQ: 1.0.0
 
 SMODS.Atlas {
     key = "modicon",
