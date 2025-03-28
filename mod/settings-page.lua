526e7d548b0205773a1aa1033a4be4cf37714d33
janw4ld <janw4ld@tutanota.com>
2025-03-29 00:40:54 +0200

    add state preview to the settings page

--- mod/settings-page.lua
+++ mod/settings-page.lua
@@ -1,16 +1,64 @@
 local layout = require("typist.mod.layout")
 local tu = require("typist.lib.tblutils")
 
-local layout_changed = false
-G.FUNCS.__typist_write_layout = function(x) -- layout change UI callback
-  local l = x.to_val
-  love.filesystem.write("typist-layout", l)
-  print('`typist-layout` set to: "' .. l .. '"')
-  layout_changed = l ~= layout.current_layout
+local M = {}
+
+-- TODO: insert a tab in the settings menu instead of an extra menu just for typist
+M.insert_settings_page_button = function(options_menu_ui)
+  table.insert(
+    options_menu_ui.nodes[1].nodes[1].nodes[1].nodes,
+    #options_menu_ui.nodes[1].nodes[1].nodes[1].nodes + 1,
+    UIBox_button {
+      minw = 5,
+      button = "__typist_draw_settings_page",
+      label = { "Typist Settings" },
+      colour = { 0.643, 0.404, 0.776, 1 }, -- #a467c6
+    }
+  )
+  return options_menu_ui
 end
 
-G.FUNCS.__typist_settings_page_ui = function()
-  local tabs = create_tabs {
+G.FUNCS.__typist_draw_settings_page = function()
+  G.FUNCS.overlay_menu {
+    definition = create_UIBox_generic_options {
+      back_func = "options",
+      contents = { M.settings_page_ui() },
+    },
+    config = { offset = { x = 0, y = 10 } },
+  }
+end
+
+local dynamic_ui_text = {}
+M.settings_page_ui = function()
+  local function create_dynamic_textbox(name, initial_value)
+    dynamic_ui_text[name] = type(initial_value) == "function" and initial_value()
+      or initial_value
+      or dynamic_ui_text[name]
+      or ""
+    return {
+      n = G.UIT.R,
+      config = { align = "cm", padding = 0 },
+      nodes = {
+        {
+          n = G.UIT.O,
+          config = {
+            id = "__typist_" .. name,
+            object = DynaText {
+              string = { { ref_table = dynamic_ui_text, ref_value = name } },
+              colours = { G.C.RED },
+              bump = true,
+              silent = true,
+              scale = 0.45,
+            },
+          },
+        },
+      },
+    }
+  end
+
+  G.FUNCS.__typist_update_and_save_settings_state { to_val = layout.current_layout }
+
+  return create_tabs {
     snap_to_nav = true,
     tabs = {
       {
@@ -34,82 +82,65 @@ G.FUNCS.__typist_settings_page_ui = function()
                 scale = 0.8,
                 w = 4,
                 options = layout.builtin_layouts,
-                opt_callback = "__typist_write_layout",
+                opt_callback = "__typist_update_and_save_settings_state",
                 current_option = tu.list_index_of(layout.builtin_layouts, layout.current_layout),
               },
+              create_dynamic_textbox("active_layout", "active layout: " .. layout.current_layout),
+              create_dynamic_textbox("restart_required"),
             },
           }
         end,
       },
     },
   }
+end
+
+local restart_required
+local should_notify_of_layout_change
+G.FUNCS.__typist_update_and_save_settings_state = function(x) -- layout change UI callback
+  local l = x.to_val
+  love.filesystem.write("typist-layout", l)
+
+  restart_required = l ~= layout.current_layout
+  should_notify_of_layout_change = restart_required
+
+  dynamic_ui_text.restart_required = "restart required: " .. (restart_required and "yes" or "no")
+end
+
+-- TODO: maybe don't rely on `require` to generate a static layout table, and allow changing the layout on the fly instead?
+M.draw_layout_change_notification = function()
+  if not should_notify_of_layout_change then return end
 
   G.FUNCS.overlay_menu {
-    definition = create_UIBox_generic_options {
-      back_func = "options",
-      contents = { tabs },
-    },
-    config = { offset = { x = 0, y = 10 } },
-  }
-end
-
-local original_create_UIBox_options = create_UIBox_options
--- TODO: insert a tab in the settings menu instead of an extra menu just for typist
-function create_UIBox_options()
-  local contents = original_create_UIBox_options()
-
-  local button = UIBox_button {
-    minw = 5,
-    button = "__typist_settings_page_ui",
-    label = { "Typist Settings" },
-    colour = { 0.643, 0.404, 0.776, 1 }, -- #a467c6
-  }
-
-  table.insert(
-    contents.nodes[1].nodes[1].nodes[1].nodes,
-    #contents.nodes[1].nodes[1].nodes[1].nodes + 1,
-    button
-  )
-
-  return contents
-end
-
--- add layout change notification on menu exit
--- TODO: maybe don't rely on \`require\` to generate a static layout table, and allow changing the layout on the fly instead?
-local original_exit_overlay_menu = G.FUNCS.exit_overlay_menu
-G.FUNCS.exit_overlay_menu = function(...)
-  original_exit_overlay_menu(...)
-
-  if layout_changed then
-    layout_changed = false
-
-    G.FUNCS.overlay_menu {
-      definition = {
-        n = G.UIT.ROOT,
-        config = { align = "cm", colour = G.C.CLEAR },
-        nodes = {
-          {
-            n = G.UIT.R,
-            config = {
-              align = "cm",
-              colour = G.C.BLACK,
-              padding = 0.2,
-              r = 0.1,
-            },
-            nodes = {
-              {
-                n = G.UIT.T,
-                config = {
-                  text = "typist layout changed, a restart is required for the change to take effect.",
-                  scale = 0.4,
-                  colour = G.C.RED,
-                },
+    definition = {
+      n = G.UIT.ROOT,
+      config = { align = "cm", colour = G.C.CLEAR },
+      nodes = {
+        {
+          n = G.UIT.R,
+          config = {
+            align = "cm",
+            colour = G.C.BLACK,
+            padding = 0.2,
+            r = 0.1,
+          },
+          nodes = {
+            {
+              n = G.UIT.T,
+              config = {
+                text = "typist layout changed, a restart is required for this change to take effect.",
+                scale = 0.4,
+                colour = G.C.RED,
               },
             },
           },
         },
       },
-      config = { offset = { x = 0, y = -0.5 } },
-    }
-  end
+    },
+    config = { offset = { x = 0, y = -0.5 } },
+  }
+
+  should_notify_of_layout_change = false
 end
+
+return M
