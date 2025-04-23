2071d2c6400d050bbf9a7493b7cce74b398b2aef
kasimeka <kasimeka@tuta.io>
2025-04-23 12:15:09 +0200

    compat: init `WilsontheWolf/DebugPlus`

--- mod/init.lua
+++ mod/init.lua
@@ -8,8 +8,6 @@ print(layout.tostring())
 G.SETTINGS.__typist = G.SETTINGS.__typist or {}
 G.SETTINGS.__typist.card_hover_duration = G.SETTINGS.__typist.card_hover_duration or 10
 
-require("typist.compat.init")()
-
 -- pseudo-CardArea object to manipulate jokers and consumable as if they're one hand
 G.__typist_TOP_AREA = setmetatable({}, { __index = { __typist_top_area = true } })
 return function(Controller, key) -- order defines precedence
